f1a0e94d355ebc1895a578cd72490c79f2409fcd
nikhilpaspuleti20 <116423567+nikhilpaspuleti20@users.noreply.github.com>
2022-12-01 10:09:43 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,20 @@
+'''
+DATE=23rd nov 2022
+DAY= thursday
+TOPIC=variables 
+AUTHOR= nikhilpaspuleti
+'''
+a=10
+_b=20
+A=60
+c_4f=50
+z5=100
+print(a,_b,A,c_4f,z5)   #10 20 60 50 100
+print('nikhil'.isidentifier()) #true
+print('3nikhil'.isidentifier()) #false
+print('_nikhil'.isidentifier()) #true
+print('hello nikh\'il') #hello poo'ja
+print("hello nikh'il") #hello poo'ja
+print('nikhilpaspuleti '*5)
+print('''abcdefghijklmnopqrstuvwxyzmmmmmmmmmmmmmmmmmmmmmmmmmmmmmvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvmkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+hhhhhhhhhhhhh ''')
